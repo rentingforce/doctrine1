0a9cd08f4ae92e281693cee09d35786ecac2f323
Andreas Bernhard <andreas@bernhard.im>
2016-01-12 11:55:52 +0100

    Fixed long identifiers (make it short by md5)

--- lib/Doctrine/Connection.php
+++ lib/Doctrine/Connection.php
@@ -1633,13 +1633,7 @@ abstract class Doctrine_Connection extends Doctrine_Configurable implements Coun
 
         // If the final length is greater than 64 we need to create an abbreviated fk name
         if (strlen(sprintf($format, $generated)) > $maxLength) {
-            $generated = '';
-
-            foreach ($parts as $part) {
-                $generated .= $part[0];
-            }
-
-            $name = $generated;
+            $generated = md5($generated);
         } else {
             $name = $generated;
         }
